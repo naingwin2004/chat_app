e20101a15520decbf9586ae4c10d2856d5011caf
naingwin2004 <naingwin.dev@gmail.com>
2024-12-08 18:51:59 +0630

    initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<button class="btn">Button</button>
+		</div>
+	);
+};
+
+export default App;
