82d0a397c649f297ad9e0f5be910309fbe69c37e
naingwin2004 <naingwin.dev@gmail.com>
2024-12-27 14:36:09 +0630

    allow cross-site cookies & netlify.toml add

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -7,7 +7,11 @@ export const generateToken = (userId, res) => {
 
 	res.cookie("jwt", token, {
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 day
+		httpOnly: true, 
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none", // Allow cross-site cookies
 	});
+	
 
 	return token;
 };
